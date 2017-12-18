12b033beb9e80a5b5da42207372fb6637e9a9bc1
pierrecaruyer <pierrecaruyer@gmail.com>
2017-12-18 12:32:32 +0100

    cleaning up again

--- server.js
+++ server.js
@@ -1,48 +1,34 @@
-var http = require('http')
-var fs = require('fs')
-var app = require('express')()
-var url = require('url')
-var multer = require('multer')
-var io = require('socket.io').listen(server)
-
-const ONE_MINUTE = 60000
-const SERVER_PORT = 3000
-const API_MUSIC = '/uploads/music/' //Must match with the action field of the client's form
-const VALID_UPLOAD_DIR = './uploads'
+const fs = require('fs')
+const app = require('express')()
+const url = require('url')
+const server = require('http').createServer(app)
+const io = require('socket.io').listen(server)
+const API_MUSIC = '/uploads/music/' //Must match the action field of the client's form
+const VALID_UPLOAD_DIR = './uploads/'
 const PUBLIC_DIRECTORY = './public/'
-const UPLOAD_FIELDNAME = 'music-upload' //Must match with the name field of the file input in the client's form
+const UPLOAD_FIELDNAME = 'music-upload' //Must match with name field of the file input in the client's form
 
-var completed_uploads = {}
-const server = http.createServer(app)
-
-server.listen(SERVER_PORT, () => poke(VALID_UPLOAD_DIR))
-server.on('close', () => delete_dir_files(VALID_UPLOAD_DIR)) //deleting uploaded files to clear a bit space
-
-const delete_dir_files = dir =>
-  fs.readdir(dir, (err, files) =>
-    files.forEach(file => fs.unlink(dir + '/' + file)))
+var songs = {}
+server.listen(3000, () => poke(VALID_UPLOAD_DIR))
 
 const poke = dir => fs.access(dir, fs.F_OK, err => { if(err) fs.mkdir(dir) })
+const delete_dir_files = dir =>
+  fs.readdir(dir, (err, files) =>
+   files.forEach(file => fs.unlink(dir + file)))
 
-const upload = multer({
+const upload = require('multer')({
   dest: VALID_UPLOAD_DIR,
   limits: { fieldNameSize: 999999999, fieldSize: 999999999 },
   includeEmptyFields: true,
   inMemory: true,
 }).single(UPLOAD_FIELDNAME)
 
-const restore_filename = file => 
-  fs.rename(file.destination + '/' + file.filename, file.destination + '/' + file.originalname)
-
 app.post(API_MUSIC, (req,res) => {
   poke(VALID_UPLOAD_DIR)
   upload(req, res, err => {
-    if(err) return res.status(500).end("Error occured while uploading file")
-    if(req.file) {
-      restore_filename(req.file)
-      completed_uploads[req.connection.remoteAddress] = { dir: VALID_UPLOAD_DIR, name: req.file.originalname }
-      app.emit('upload-completed', req.file)
-    }
+    if(err || !req.file) return res.status(500).end("Error occured while uploading file")
+    fs.rename(req.file.destination + req.file.filename, req.file.destination + req.file.originalname)
+    songs[req.connection.remoteAddress] = { dir: VALID_UPLOAD_DIR, name: req.file.originalname }
     res.redirect('back') //Here the client's page refreshes after the upload is done
   })
 })
@@ -68,21 +54,12 @@ const loadMidiFileContent = (socket, dir, file) => {
 }
 
 io.sockets.on('connection', socket => {
-  const socket_address = socket.request.connection.remoteAddress
-  console.log('New connection at : ' + socket_address)
+  const address = socket.request.connection.remoteAddress
+  console.log('New connection at : ' + address)
+  socket.on('clientException', error => { console.log('Client at ' + address + 'just got the following error' + error.desc) })
+  socket.on('disconnect', message => { console.log('Goodbye ' + address) })
 
-  /**
-   * Right after a new socket connects to the server, the socket address is checked, if it is found in completed_uploads,
-   * it means the client is reconnecting to the server (because of the web browser refreshing after the POST request)
-   * after having uploaded his file to the server.
-   * The content of the file is then computed in order to be sent as a string containing the midi data.
-  **/
-  if(!completed_uploads[socket_address]) return
-  const dir = completed_uploads[socket_address].dir + '/', file = completed_uploads[socket_address].name
-
-  fs.access(dir + file, fs.F_OK, () => loadMidiFileContent(socket, dir, file))
-  delete completed_uploads[socket_address] //one-time-usage strcuture
-
-  socket.on('clientException', error => { console.log('Client ' + error.desc) })
-  socket.on('disconnect', message => { console.log('Goodbye ' + socket_address) })
+  if(!songs[address]) return //if this client has not uploaded a song
+  fs.access(songs[address].dir + songs[address].name, fs.F_OK, () => loadMidiFileContent(socket, songs[address].dir, songs[address].name))
+  delete songs[address]
 })

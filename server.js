42b668ddb73a70cb300dca58f212e88a0d4ddca3
pierrecaruyer <pierrecaruyer@gmail.com>
2018-03-21 17:07:25 +0100

    promisified

--- server.js
+++ server.js
@@ -1,65 +1,72 @@
 const fs = require('fs')
-const app = require('express')()
 const url = require('url')
+const multer = require('multer')
+const app = require('express')()
+const UPLOAD_DIR = './uploads/'
+const UPLOAD_FIELDNAME = 'music-upload' //Must match with name field of the file input in the client's form
 const server = require('http').createServer(app)
 const io = require('socket.io').listen(server)
-const API_MUSIC = '/uploads/music/' //Must match the action field of the client's form
-const UPLOAD_DIR = './uploads/'
-const PUBLIC_DIRECTORY = './public/'
-const UPLOAD_FIELDNAME = 'music-upload' //Must match with name field of the file input in the client's form
-
 var songs = {}
-server.listen(3000, () => fs.access(UPLOAD_DIR, fs.F_OK, err => { if(err) fs.mkdir(UPLOAD_DIR) }))
-
-const upload = require('multer')({
-  dest: UPLOAD_DIR,
-  limits: { fieldNameSize: 999999999, fieldSize: 999999999 },
-  includeEmptyFields: true,
-  inMemory: true,
-}).single(UPLOAD_FIELDNAME)
-
-app.post(API_MUSIC, (req,res) => {
-  fs.access(UPLOAD_DIR, fs.F_OK, err => { if(err) fs.mkdir(UPLOAD_DIR) })
-  upload(req, res, err => {
-    if(err || !req.file) return res.status(500).end("Error occured while uploading file")
-    fs.rename(req.file.destination + req.file.filename, req.file.destination + req.file.originalname)
-    songs[req.connection.remoteAddress] = { dir: UPLOAD_DIR, name: req.file.originalname }
-    res.redirect('back') //Here the client's page refreshes after the upload is done
-  })
+server.listen(80, () => fs.exists(UPLOAD_DIR, exists => { if(!exists) fs.mkdir(UPLOAD_DIR) }))
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => callback(null, UPLOAD_DIR),
+  filename: (req, file, callback) => {
+    songs[req.connection.remoteAddress] = { dir: UPLOAD_DIR, name: file.originalname }
+    callback(null, file.originalname)
+  }
 })
-.use((req, res, next) => {
-  var options = {
-    root: PUBLIC_DIRECTORY,
+const upload = multer({storage: storage}).single(UPLOAD_FIELDNAME)
+
+app.post('/uploads/music/', (req,res) => {
+  fs.access(UPLOAD_DIR, fs.F_OK, err => { if(err) fs.mkdir(UPLOAD_DIR) })
+  upload(req, res, err => res.redirect('back')) //Here the client's page refreshes after the upload is done
+}).use((req, res, next) => {
+  let options = {
+    root: './public/',
     dotfiles: 'deny',
     headers: { 'x-timestamp': Date.now(), 'x-sent': true }
   }
   res.sendFile(url.parse(req.url).pathname, options, err => next(err))
 })
 
-const toArray = buf => {
-  var view = []
-  for (var i = 0; i < buf.length; ++i)
-      view.push(buf[i])
-  return view;
-}
+const readFileAsync = (path, socket) => new Promise((resolve, reject) =>
+  fs.readFile(path, (err, content) =>
+    err ? reject(err) : resolve({file: path, endpoint: socket, data: content})
+))
+
+const extractMidiData = args => new Promise(resolve =>
+  resolve({name: args.file, socket: args.endpoint, midi: new Array(args.data.length)
+    .fill(undefined)
+    .map((value, index) => args.data[index])
+    .map(value => String.fromCharCode(value & 255))
+    .join('')
+  }))
+
+const sendMidiData = args => new Promise(resolve => {
+  args.socket.emit('file-parsed', {midi: args.midi, name: args.name})
+  resolve(args)
+})
+
+const cleanupMidiData = args => new Promise(resolve => {
+  fs.unlink(args.name)
+  delete songs[args.socket.request.connection.remoteAddress]
+})
 
 const loadMidiFileContent = (socket, dir, file) => {
   console.log('Starting to load ' + file + '\'s midi content')
-  fs.readFile(dir + file, (err, content) => {
-    if(err) return
-    var computedMidiData = toArray(content)
-      .map(value => String.fromCharCode(value & 255))
-      .join('')
-    socket.emit('file-parsed', { midi: computedMidiData, name: file }) //Sending the data to the client
-    fs.unlink(dir + file) //deleting the uploaded file to free up some space on the server's hard disk
-    delete songs[socket.request.connection.remoteAddress]
-  })
+  readFileAsync(dir + file, socket)
+    .then(extractMidiData, console.error)
+    .then(sendMidiData, console.error)
+    .then(cleanupMidiData)
+    .catch(console.error)
 }
 
 io.sockets.on('connection', socket => {
   const address = socket.request.connection.remoteAddress
   console.log('New connection at : ' + address)
-  socket.on('clientException', error => { console.log('Client at ' + address + 'just got the following error' + error.desc) })
-  socket.on('disconnect', message => { console.log('Goodbye ' + address) })
-  if(songs[address]) fs.access(songs[address].dir + songs[address].name, fs.F_OK, () => loadMidiFileContent(socket, songs[address].dir, songs[address].name))
+  socket.on('clientException', error => console.log('Client at ' + address + 'just got the following error' + error.desc))
+  socket.on('disconnect', message => console.log('Goodbye ' + address))
+  if(songs[address])
+    fs.access(songs[address].dir + songs[address].name, fs.F_OK, () =>
+      loadMidiFileContent(socket, songs[address].dir, songs[address].name))
 })
